35810de246ae6e961c94234e987d13540ca00487
Marzan A <marzan.marss@gmail.com>
2025-08-26 01:52:50 +0300

    add new endpoint and account payloads

---
+++ payloads/accounts_payloads.py
@@ -0,0 +1,8 @@
+class Payloads:
+
+    @staticmethod
+    def create_entity_ewallet(get_currency_id):
+        return {
+            'currency_id': get_currency_id
+        }
+    
--- services/accounts/endpoints.py
+++ services/accounts/endpoints.py
@@ -2,5 +2,12 @@ import os
 
 class Endpoints:
 
-    total_amount = lambda self, cur_id: f"{os.getenv('HOST')}/accounts/total-amounts/{cur_id}"
+    total_amount = lambda self, cur_id: f"{os.getenv('HOST')}/accounts/total-amounts/{cur_id}/"
+
     accouts_ewallet = f"{os.getenv('HOST')}/accounts/ewallets/"
+
+    ewallet_currencies = f"{os.getenv('HOST')}/accounts/ewallets/currencies/"
+
+    entity_ewallets = lambda self, cur_id: f"{os.getenv('HOST')}/ewallets/{cur_id}/"
+
+    create_entity_ewallet = f"{os.getenv('HOST')}/accounts/ewallets"
