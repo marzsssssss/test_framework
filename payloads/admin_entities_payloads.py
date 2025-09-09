0e6d1e865369acd49f586ae60d466d0e47a67d37
Marzan A <marzan.marss@gmail.com>
2025-09-10 00:50:16 +0300

    negative tests

--- payloads/admin_entities_payloads.py
+++ payloads/admin_entities_payloads.py
@@ -14,44 +14,10 @@ class Payloads:
             "hidden": fake.boolean()
         }
     
-    #Обязательные поля
+    #Required 
     @staticmethod
     def without_field(field, ewallet_id):
         payload = Payloads.adjust_balance(ewallet_id)
         payload_copy = payload.copy()
         payload_copy.pop(field, None)
         return payload_copy
-    
-    @staticmethod
-    def field_test(field, ewallet_id):
-        payload = Payloads.adjust_balance(ewallet_id)
-        payloads = []
-
-       
-        negative_values = {
-            "invoice_direction": [
-                fake.pystr(min_chars=4, max_chars=4), 
-                fake.random_number(digits=3),                                       
-            ],
-            "account_ewallet_id": [          
-                fake.random_number(digits=3),
-                fake.boolean()           
-            ],
-            "amount": [
-                -10,                                   
-                "ten",                                  
-                0,                                       
-            ],
-            "hidden": [                                  
-                "ten",                                  
-                fake.uuid4(),
-                '123'                                       
-            ]
-        }
-
-        for value in negative_values.get(field, []):
-            pl = payload.copy()
-            pl[field] = value
-            payloads.append(pl)
-
-        return payloads
--- tests/admin_entities/test_admin_entities_negative.py
+++ tests/admin_entities/test_admin_entities_negative.py
@@ -12,19 +12,17 @@ fake = Faker()
 @allure.feature('Test Services - Admin Entities - Negative')
 class TestAdminEntitiesNegative(BaseTest):
 
-    name = os.getenv('UN_NAME')
-    json = lambda ewallet_id: Payloads.adjust_balance(ewallet_id)
-
     @allure.title('Negative Tests Accounts Headers')
     @pytest.mark.parametrize(
-        ('headers', 'expected_status', 'name'), [
-            (Headers.empty_token, 401, name),
-            (Headers.invalid_token, 401, name),
-            (Headers.missing_token, 401, name)
+        ('headers', 'expected_status'), [
+            (Headers.empty_token, 401),
+            (Headers.invalid_token, 401),
+            (Headers.missing_token, 401)
         ]
     )
-    def test_admin_entities_headers(self, headers, expected_status, name, get_ewallet_id):
+    def test_admin_entities_headers(self, headers, expected_status,  get_ewallet_id):
         json = Payloads.adjust_balance(get_ewallet_id)
+        name = os.getenv('UN_NAME')
         self.admin_entities_api_negative.logger.info(f'Test launch Negative Headers  - GET - /admin/entities/accounts/ Negative, headers -  {headers} - expected status - {expected_status}')
         self.admin_entities_api_negative.get_entities_accounts(name, headers, expected_status)
         self.admin_entities_api_negative.logger.info(f'Test launch Negative Headers  - POST - /admin/entities/accounts/adjust-balance Negative, headers -  {headers} - expected status - {expected_status}')
@@ -61,19 +59,76 @@ class TestAdminEntitiesNegative(BaseTest):
         self.admin_entities_api_negative.post_entities_accounts_adjust(json_required, headers, expected_status)
 
 
-    @allure.title('Test POST - Invalid Field Values')
+
+    @allure.title('Test POST - /admin/entities/accounts/adjust-balance - Invalid invoice_direction')
     @pytest.mark.parametrize(
-        'field', [
-            'invoice_direction',
-            'account_ewallet_id',
-            'amount',
-            'hidden'
+        ('invoice_direction', 'expected_status'), [
+            ({"invoice_direction": fake.pystr(min_chars=4, max_chars=4)}, 422),
+            ({"invoice_direction": fake.random_number(digits=3)}, 422),
+            ({"invoice_direction": ""}, 422),
         ]
     )
-    def test_invalid_field_values(self, field, get_ewallet_id, headers=Headers.basic):
-        payload_keys = Payloads().field_test(field, get_ewallet_id)
-        for payload in payload_keys:
-            self.admin_entities_api_negative.logger.info(
-                f'Test launch Invalid Field Value - POST, field: {field}, payload: {payload}'
-            )
-            self.admin_entities_api_negative.post_entities_accounts_adjust(payload, headers, expected_status=422)   
+    def test_invalid_invoice_values(self, invoice_direction, expected_status, get_ewallet_id, headers=Headers.basic):
+        base = Payloads.adjust_balance(get_ewallet_id)
+        payload = {**base, **invoice_direction}
+        self.admin_entities_api_negative.logger.info(
+            f'Test POST - /admin/entities/accounts/adjust-balance - Invalid invoice_direction, expected_status: {expected_status}, invoice_direction - {invoice_direction}'
+        )
+        self.admin_entities_api_negative.post_entities_accounts_adjust(payload, headers, expected_status)
+
+
+
+
+    @allure.title('Test POST - /admin/entities/accounts/adjust-balance - Invalid account_ewallet_id')
+    @pytest.mark.parametrize(
+        ('account_ewallet_id', 'expected_status'), [
+            ({"account_ewallet_id": fake.random_number(digits=3)}, 422),
+            ({"account_ewallet_id": fake.boolean()}, 422),
+            ({"account_ewallet_id": fake.uuid4()}, 404),
+            ({"account_ewallet_id": ""}, 422)
+        ]
+    )
+    def test_invalid_account_values(self, account_ewallet_id, expected_status, get_ewallet_id, headers=Headers.basic):
+        base = Payloads.adjust_balance(get_ewallet_id)
+        payload = {**base, **account_ewallet_id}
+        self.admin_entities_api_negative.logger.info(
+            f'Test POST - /admin/entities/accounts/adjust-balance - Invalid account_ewallet_id, expected_status: {expected_status}, account_ewallet_id - {account_ewallet_id}'
+        )
+        self.admin_entities_api_negative.post_entities_accounts_adjust(payload, headers, expected_status)
+
+    
+
+    @allure.title('Test POST - /admin/entities/accounts/adjust-balance - Invalid amount')
+    @pytest.mark.parametrize(
+        ('amount', 'expected_status'), [
+            ({"amount": -10}, 422),
+            ({"amount": 'ten'}, 422),
+            ({"amount": 0}, 422),
+            ({"amount": fake.boolean()}, 422)
+        ]
+    )
+    def test_invalid_amount_values(self, amount, expected_status, get_ewallet_id, headers=Headers.basic):
+        base = Payloads.adjust_balance(get_ewallet_id)
+        payload = {**base, **amount}
+        self.admin_entities_api_negative.logger.info(
+            f'Test POST - /admin/entities/accounts/adjust-balance - Invalid amount, expected_status: {expected_status}, account_ewallet_id - {amount}'
+        )
+        self.admin_entities_api_negative.post_entities_accounts_adjust(payload, headers, expected_status)
+
+
+
+    @allure.title('Test POST - /admin/entities/accounts/adjust-balance - Invalid hidden')
+    @pytest.mark.parametrize(
+        ('hidden', 'expected_status'), [
+            ({"hidden": "ten"}, 422),
+            ({"hidden": fake.uuid4()}, 422),
+            ({"hidden": 123}, 422),
+        ]
+    )
+    def test_invalid_hidden_values(self, hidden, expected_status, get_ewallet_id, headers=Headers.basic):
+        base = Payloads.adjust_balance(get_ewallet_id)
+        payload = {**base, **hidden}
+        self.admin_entities_api_negative.logger.info(
+            f'Test POST - /admin/entities/accounts/adjust-balance - Invalid hidden, expected_status: {expected_status}, account_ewallet_id - {hidden}'
+        )
+        self.admin_entities_api_negative.post_entities_accounts_adjust(payload, headers, expected_status)
